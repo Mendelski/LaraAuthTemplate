525d0f9c055282dd87991dde29cf154972d8284d
Erick Mendelski <emendelski@yuzer.com.br>
2023-12-24 13:11:20 -0300

    Add seeders

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Creating Super Admin User
+        $superAdmin = User::create([
+            'name' => 'Erick Mendelski',
+            'email' => 'erick.mendelski@gmail.com',
+            'password' => Hash::make('laravel123')
+        ]);
+        $superAdmin->assignRole('Super Admin');
+
+        // Creating Admin User
+        $admin = User::create([
+            'name' => 'admin user',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('laravel123')
+        ]);
+        $admin->assignRole('Admin');
+
+        // Creating Product Manager User
+        $productManager = User::create([
+            'name' => 'product Manager User',
+            'email' => 'pmuser@emial.com',
+            'password' => Hash::make('laravel123')
+        ]);
+        $productManager->assignRole('Product Manager');
+    }
+}
