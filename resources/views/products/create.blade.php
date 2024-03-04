9b70f7466bf488a8893ee5ea65ee0e2d217c485f
Erick Mendelski <erick.mendelski@gmail.com>
2024-03-04 12:31:22 -0300

    Fix some texts

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -11,7 +11,7 @@
                         Add New Product
                     </div>
                     <div class="float-end">
-                        <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
+                        <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">&larr; Voltar</a>
                     </div>
                 </div>
                 <div class="card-body">
@@ -19,7 +19,7 @@
                         @csrf
 
                         <div class="mb-3 row">
-                            <label for="name" class="col-md-4 col-form-label text-md-end text-start">Name</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end text-start">Nome</label>
                             <div class="col-md-6">
                                 <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
                                 @if ($errors->has('name'))
@@ -29,7 +29,7 @@
                         </div>
 
                         <div class="mb-3 row">
-                            <label for="description" class="col-md-4 col-form-label text-md-end text-start">Description</label>
+                            <label for="description" class="col-md-4 col-form-label text-md-end text-start">Descrição</label>
                             <div class="col-md-6">
                                 <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description') }}</textarea>
                                 @if ($errors->has('description'))
